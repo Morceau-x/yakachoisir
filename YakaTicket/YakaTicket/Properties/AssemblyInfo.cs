307e15749e537072eff92e32c56c9a2f4144d812
Sangbin Lee <sangbin.lee@epita.fr>
2018-04-22 17:52:25 +0200

    added login page to the website

--- YakaTicket/YakaTicket/Properties/AssemblyInfo.cs
+++ YakaTicket/YakaTicket/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("abe878b6-591c-4a8e-beb0-ef0b2d062d29")]
+[assembly: Guid("260042f0-407c-4fba-a270-80cca7da1996")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ YakaTicket/YakaTicket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YakaTicket.Startup))]
+namespace YakaTicket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
