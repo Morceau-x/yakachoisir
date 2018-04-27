3c40fda0892b7a06d7710e0f32f98039b57059ee
Hedgestock <pamarthur@free.fr>
2018-04-27 15:13:22 +0200

    [EDIT] merged css files and added generated code.

--- YakaTicket/YakaTicket/App_Start/BundleConfig.cs
+++ YakaTicket/YakaTicket/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace YakaTicket
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/customMain.css"));
         }
     }
 }
