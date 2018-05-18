d9bb16cfa62d3d04c650ec263616f04a89b0610f
TrakerCookie <vincent.boulay@epita.fr>
2018-05-18 22:25:43 +0200

    Add QR scanner

--- YakaAppli/app/src/main/java/fr/epita/yakaappli/MainActivity.kt
+++ YakaAppli/app/src/main/java/fr/epita/yakaappli/MainActivity.kt
@@ -5,6 +5,13 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.zxing.integration.android.IntentIntegrator
+import android.content.Intent
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentResult
+
+
+
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -21,10 +28,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (clickedView != null) {
             when (clickedView.id) {
                 R.id.btn_scan -> {
+                    scanIntegrator.setOrientationLocked(false)
+                    scanIntegrator.initiateScan()
                 }
                 else -> {
                 }
             }
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+        //retrieve scan result
+        val scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent)
+        if (scanningResult != null) {
+            //we have a result
+            if(scanningResult.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + scanningResult.getContents(), Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            Toast.makeText(this, "Not found", Toast.LENGTH_LONG).show()
+            super.onActivityResult(requestCode, resultCode, intent)
         }
     }
 }
