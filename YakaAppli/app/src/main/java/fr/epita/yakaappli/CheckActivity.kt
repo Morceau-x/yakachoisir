d6b3e922c93133df407358dc4ddda2d3f8618167
TrakerCookie <vincent.boulay@epita.fr>
2018-05-20 21:59:15 +0200

    [APPLI] Split activities

---
+++ YakaAppli/app/src/main/java/fr/epita/yakaappli/CheckActivity.kt
@@ -0,0 +1,53 @@
+package fr.epita.yakaappli
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_check.*
+
+class CheckActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check)
+
+        btn_scan.setOnClickListener(this@CheckActivity)
+        val originIntent = intent
+        val id = originIntent.getStringExtra("ID")
+    }
+
+    var scanIntegrator = IntentIntegrator(this)
+
+    override fun onClick(clickedView: View?) {
+        if (clickedView != null) {
+            when (clickedView.id) {
+                R.id.btn_scan -> {
+                    scanIntegrator.setOrientationLocked(false)
+                    scanIntegrator.initiateScan()
+                }
+                else -> {
+                }
+            }
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+        //retrieve scan result
+        val scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent)
+        if (scanningResult != null) {
+            //we have a result
+            if(scanningResult.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + scanningResult.getContents(), Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            Toast.makeText(this, "Not found", Toast.LENGTH_LONG).show()
+            super.onActivityResult(requestCode, resultCode, intent)
+        }
+    }
+}
--- YakaAppli/app/src/main/java/fr/epita/yakaappli/MainActivity.kt
+++ YakaAppli/app/src/main/java/fr/epita/yakaappli/MainActivity.kt
@@ -1,17 +1,10 @@
 package fr.epita.yakaappli
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import com.google.zxing.integration.android.IntentIntegrator
-import android.content.Intent
-import android.widget.Toast
-import com.google.zxing.integration.android.IntentResult
-
-
-
-
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -19,17 +12,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_scan.setOnClickListener(this@MainActivity)
+        btn_connect.setOnClickListener(this@MainActivity)
     }
-
-    var scanIntegrator = IntentIntegrator(this)
 
     override fun onClick(clickedView: View?) {
         if (clickedView != null) {
             when (clickedView.id) {
-                R.id.btn_scan -> {
-                    scanIntegrator.setOrientationLocked(false)
-                    scanIntegrator.initiateScan()
+                R.id.btn_connect -> {
+                    val explicitIntent = Intent(this@MainActivity, CheckActivity::class.java)
+                    val message = "Identifiant"
+                    explicitIntent.putExtra("ID", message)
+                    startActivity(explicitIntent)
                 }
                 else -> {
                 }
@@ -37,20 +30,4 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
-        //retrieve scan result
-        val scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent)
-        if (scanningResult != null) {
-            //we have a result
-            if(scanningResult.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, "Scanned: " + scanningResult.getContents(), Toast.LENGTH_LONG).show()
-            }
-        }
-        else{
-            Toast.makeText(this, "Not found", Toast.LENGTH_LONG).show()
-            super.onActivityResult(requestCode, resultCode, intent)
-        }
-    }
 }
