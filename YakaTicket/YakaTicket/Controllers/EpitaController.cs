db4d4e11524067546df575baf142819c6c5168ab
Maxime L'Hermite <maxime.lhermite@epita.fr>
2018-05-22 14:26:40 +0200

    [EDIT] tests for epita connect

---
+++ YakaTicket/YakaTicket/Controllers/EpitaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace YakaTicket.Controllers
+{
+    public class EpitaController : Controller
+    {
+        // GET: Epita
+        public ActionResult Index()
+        {
+            Request.InputStream.Position = 0;
+            var input = new StreamReader(Request.InputStream).ReadToEnd();
+            ViewBag.text = input;
+            return View();
+        }
+    }
+}
