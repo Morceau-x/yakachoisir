b5be773b85c0ae4751526a4d49fcca2eb97d1b02
TrakerCookie <vincent.boulay@epita.fr>
2018-05-24 20:39:57 +0200

    Change style for event buttons

--- YakaAppli/app/src/main/java/fr/epita/yakaappli/EventsActivity.kt
+++ YakaAppli/app/src/main/java/fr/epita/yakaappli/EventsActivity.kt
@@ -8,6 +8,7 @@ import kotlin.collections.ArrayList
 import kotlinx.android.synthetic.main.activity_events.*
 import android.widget.Toast
 import android.R.interpolator.linear
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import android.provider.FontsContract
@@ -15,6 +16,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import android.graphics.Typeface
+
+
 
 
 
@@ -33,11 +37,16 @@ class EventsActivity : AppCompatActivity() {
 
 
         for (e in events) {
-            val linearLayout : LinearLayout = findViewById(R.id.rootContainer);
+            val linearLayout : LinearLayout = findViewById(R.id.rootContainer)
 
             var btnShow = Button(this)
             btnShow.setText(e.name)
-            btnShow.setLayoutParams(LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+            btnShow.setTextColor(resources.getColor(R.color.colorBlank))
+            btnShow.setBackgroundResource(R.drawable.button_event)
+            var params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            params.setMargins(16,16,16,16)
+            btnShow.setLayoutParams(params)
             btnShow.setOnClickListener(object : View.OnClickListener {
                 override fun onClick(v : View) {
                     val explicitIntent = Intent(this@EventsActivity, CheckActivity::class.java)
