096ab7479d40e54687beee3c65a99c746a0b19f4
Sangbin Lee <sangbin.lee@epita.fr>
2018-05-24 21:25:22 +0200

    [ADD] QRCode Writer

---
+++ YakaTicket/YakaTicket/Tools/QRCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ZXing;
+
+namespace YakaTicket.Tools
+{
+    public class QRCode
+    {
+        BarcodeWriter _barcode = null;
+
+        QRCode()
+        {
+            _barcode = new BarcodeWriter();
+        }
+
+        public void Write()
+        {
+            _barcode.Format = BarcodeFormat.QR_CODE;
+            _barcode.Write("test").Save("test.bmp");
+        }
+    }
+}
