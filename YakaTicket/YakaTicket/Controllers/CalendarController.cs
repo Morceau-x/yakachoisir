087e3b1fa6a4ddfb7d7e205438bd8f1a98ae1691
Hedgestock <pamarthur@free.fr>
2018-06-07 20:47:43 +0200

    [ADD] Added calendar controler

---
+++ YakaTicket/YakaTicket/Controllers/CalendarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DayPilot.Web.Mvc;
+using DayPilot.Web.Mvc.Enums;
+using DayPilot.Web.Mvc.Events.Calendar;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using YakaTicket.Models;
+
+namespace YakaTicket.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public ActionResult Backend()
+        {
+            return new Dpc().CallBack(this);
+        }
+
+        class Dpc : DayPilotCalendar
+        {
+            protected override void OnInit(InitArgs e)
+            {
+                UpdateWithMessage("Welcome!", CallBackUpdateType.Full);
+            }
+        }
+    }
+}
