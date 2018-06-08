72ba9293134d3a9c6bc81afa9ca9c48d9db88756
Maxime L'Hermite <maxime.lhermite@epita.fr>
2018-06-08 21:13:15 +0200

    dude

--- YakaTicket/YakaTicket/Controllers/EventsController.cs
+++ YakaTicket/YakaTicket/Controllers/EventsController.cs
@@ -51,7 +51,7 @@ namespace YakaTicket.Controllers
         [HttpPost]
         public ActionResult ViewEvent(string name)
         {
-            Database.Database.database.RequestVoid("f_approve", HttpContext.User.Identity.Name, name);
+            Database.Database.database.RequestVoid("f_approve", User.Identity.Name, name);
             return RedirectToAction("ViewEvent", "Events", new { name });
         }
 
--- database/test.sql
+++ database/test.sql
@@ -1,18 +1,21 @@
-CREATE OR REPLACE FUNCTION f_approve(login VARCHAR(256), name VARCHAR(1024))
+CREATE OR REPLACE FUNCTION f_can_approve(login VARCHAR(256), name VARCHAR(1024))
 RETURNS BOOLEAN AS
 $$
 DECLARE
 	assoc VARCHAR(1024);
+	pres_app BOOLEAN;
+	mod_app BOOLEAN;
 BEGIN
-	SELECT e.assoc INTO assoc FROM events e WHERE e.name = $2;
-	IF (NOT f_is_president(login, assoc)) THEN
-		RETURN FALSE;
-	END IF;
-	UPDATE events SET president_approved = TRUE WHERE events.name = $2;
-	IF (NOT f_is_moderator(login)) THEN
+	SELECT e.assoc, e.president_approved, e.moderator_approved INTO assoc, pres_app, mod_app FROM events e WHERE e.name = $2;
+	IF (f_is_president(login, assoc) AND pres_app = FALSE) THEN
 		RETURN TRUE;
 	END IF;
-	UPDATE events SET moderator_approved = TRUE WHERE events.name = $2;
-	RETURN TRUE;
+	IF (f_is_moderator(login) AND mod_app = FALSE) THEN
+		RETURN TRUE;
+	END IF;
+	RETURN FALSE;
+EXCEPTION
+	WHEN OTHERS THEN
+		RETURN FALSE;
 END;
 $$ LANGUAGE plpgsql;
