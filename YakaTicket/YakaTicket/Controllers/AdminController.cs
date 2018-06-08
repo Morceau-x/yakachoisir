accbc7f3bf1c6156ec34f2a1a5675e2941d85ffc
Maxime L'Hermite <maxime.lhermite@epita.fr>
2018-06-08 18:04:40 +0200

    e"gbre

--- YakaTicket/YakaTicket/Controllers/AdminController.cs
+++ YakaTicket/YakaTicket/Controllers/AdminController.cs
@@ -11,7 +11,10 @@ namespace YakaTicket.Controllers
         // GET: Admin
         public RedirectToRouteResult Index()
         {
-            return RedirectToAction("ModeratorDashboard", "Admin");
+            if (Database.Database.database.RequestBoolean("f_is_moderator", User.Identity.Name))
+                return RedirectToAction("ModeratorDashboard", "Admin");
+            else
+                return RedirectToAction("PresidentDashboard", "Admin");
         }
 
         public ActionResult ModeratorDashboard()
@@ -20,6 +23,35 @@ namespace YakaTicket.Controllers
             try
             {
                 List<object[]> table = Database.Database.database.RequestTable("f_list_mod_events", 7);
+                foreach (var row in table)
+                {
+                    var e = new Models.Event()
+                    {
+                        Name = (string)row[0],
+                        Description = (string)row[1],
+                        Premium = (bool)row[2],
+                        Begin = (DateTime)row[3],
+                        End = (DateTime)row[4],
+                        Assoc = (string)row[5],
+                        Owner = (string)row[6]
+                    };
+
+                    list.Add(e);
+                }
+            }
+            catch (Exception)
+            { }
+
+            ViewBag.list = list;
+            return View();
+        }
+
+        public ActionResult PresidentDashboard()
+        {
+            var list = new List<Models.Event>();
+            try
+            {
+                List<object[]> table = Database.Database.database.RequestTable("f_list_pres_events", 7);
                 foreach (var row in table)
                 {
                     var e = new Models.Event()
