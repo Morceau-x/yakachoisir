e1888820f747688d96d5f0329afacf677ede11c0
Maxime L'Hermite <maxime.lhermite@epita.fr>
2018-06-25 23:22:20 +0200

    tiens tu fais chier sangbin

--- database/events/events_functions.sql
+++ database/events/events_functions.sql
@@ -218,6 +218,47 @@ END;
 $$ LANGUAGE plpgsql;
 
 /*********************
+*** CAN PARTICPATE ***
+*********************/
+CREATE OR REPLACE FUNCTION f_can_participate(login VARCHAR(256), name VARCHAR(1024), price VARCHAR(1024))
+RETURNS BOOLEAN AS
+$$
+DECLARE
+	pri INTEGER;
+	ass VARCHAR(1024);
+	epita BOOLEAN;
+	ionis BOOLEAN;
+	assoc BOOLEAN;
+
+	epita_req BOOLEAN;
+	ionis_req BOOLEAN;
+	assoc_req BOOLEAN;
+BEGIN
+	IF (login IS NULL OR name IS NULL OR price IS NULL) THEN
+		RETURN FALSE;
+	END IF;
+	SELECT e.assoc INTO ass FROM events e WHERE e.name = $2;
+	SELECT p.id INTO pri FROM prices p WHERE p.event = $2 AND p.price_name = $3;
+	SELECT u.epita, u.ionis INTO epita, ionis FROM users u WHERE u.login = $1;
+	SELECT p.epita_only, p.ionis_only, p.assoc_only INTO epita_req, ionis_req, assoc_req FROM prices p WHERE p.id = pri;
+	
+	IF (EXISTS(SELECT * FROM participants p WHERE p.login = $1 AND p.event = $2)) THEN
+		RETURN FALSE;
+	END IF;
+	IF (f_is_member($1, ass) OR f_is_desk($1, ass) OR f_is_president($1, ass)) THEN
+		SELECT TRUE INTO assoc;
+	ELSE
+		SELECT FALSE INTO assoc;
+	END IF;
+
+	RETURN assoc OR (NOT assoc_req AND (epita OR (NOT epita_req AND (ionis OR (NOT ionis_req)))));
+EXCEPTION
+	WHEN OTHERS THEN
+		RETURN FALSE;
+END;
+$$ LANGUAGE plpgsql;
+
+/*********************
 ****** ADD STAFF *****
 *********************/
 CREATE OR REPLACE FUNCTION f_add_staff(login VARCHAR(256), name VARCHAR(1024))
@@ -438,8 +479,9 @@ $$ LANGUAGE plpgsql;
 *********************/
 CREATE OR REPLACE FUNCTION f_list_prices(event VARCHAR(1024))
 RETURNS SETOF price_data AS
-'SELECT p.price_name, p.price_value, count(s.id), p.max_number, p.assoc_only, p.epita_only, p.ionis_only 
+'SELECT p.price_name, p.price_value, count(s.id)::integer, p.max_number, p.assoc_only, p.epita_only, p.ionis_only 
 FROM participants s
 JOIN prices p ON p.id = s.price
-WHERE p.event = $1;'
+WHERE p.event = $1
+GROUP BY p.price_name, p.price_value, p.max_number, p.assoc_only, p.epita_only, p.ionis_only;'
 LANGUAGE SQL;
