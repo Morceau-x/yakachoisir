42f55855bbd229b60781723dda931db6405da166
TrakerCookie <vincent.boulay@epita.fr>
2018-06-26 16:44:11 +0200

    Add login to API

---
+++ YakaTicket/YakaTicket/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace YakaTicket.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [Route("api/GetLogin/{login}/{mdp}")]
+        public string Get(string login, string mdp)
+        {
+            // Authentication
+            return login;
+        }
+    }
+}
